7043e6b37128ca36119e6dd46bf051aff922ae26
Gustavo Carvalho <gustavocarvalho7b@gmail.com>
2025-07-15 16:48:42 -0300

    Postgresql

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeBarComponent } from './home-bar/home-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/src/app/home-bar/home-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-bar',
+  templateUrl: './home-bar.component.html',
+  styleUrl: './home-bar.component.scss'
+})
+export class HomeBarComponent {
+
+}
