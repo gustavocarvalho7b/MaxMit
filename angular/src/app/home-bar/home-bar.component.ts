71395f157686bae062a1d6e7691e8cfb460259e6
Gustavo Carvalho <gustavocarvalho7b@gmail.com>
2025-07-15 19:18:04 -0300

    Background

--- angular/src/app/home-bar/home-bar.component.ts
+++ angular/src/app/home-bar/home-bar.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-bar',
   templateUrl: './home-bar.component.html',
-  styleUrl: './home-bar.component.scss'
+  styleUrl: './home-bar.component.scss',
 })
 export class HomeBarComponent {
-
+  menuMax: boolean = false;
+  menuAdmin() {
+    this.menuMax = true;
+  }
 }
