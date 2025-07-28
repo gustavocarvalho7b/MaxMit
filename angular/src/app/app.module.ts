e25f07d6b5e80fe610c709c4b67582ccb5a3e1e2
Gustavo Carvalho <gustavocarvalho7b@gmail.com>
2025-07-27 21:23:29 -0300

    Menu

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeBarComponent } from './home-bar/home-bar.component';
 import { MenuAdminComponent } from './menu-admin/menu-admin.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeBarComponent,
-    MenuAdminComponent
+    MenuAdminComponent,
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
