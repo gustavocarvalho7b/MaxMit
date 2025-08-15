056ec1424f6951d7a2f6ed0ab626bfe0dcd060fa
Gustavo Carvalho <gustavocarvalho7b@gmail.com>
2025-08-15 12:03:18 -0300

    Esteira

--- angular/src/app/cardall/cardall.component.ts
+++ angular/src/app/cardall/cardall.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Directive, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cardall',
   templateUrl: './cardall.component.html',
-  styleUrl: './cardall.component.scss'
+  styleUrl: './cardall.component.scss',
 })
-export class CardallComponent {
-
-}
+export class CardallComponent {}
